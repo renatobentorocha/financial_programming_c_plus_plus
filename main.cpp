15079274fb8b131312b036a169b9cb74f59c72fb
reanatorocha <87495675+reanatorocha@users.noreply.github.com>
2022-04-12 19:56:22 -0300

    Simple Interest Rates

--- main.cpp
+++ main.cpp
@@ -1,9 +1,47 @@
 #include <iostream>
 #include "SimpleInterestRates/SimpleInterestRates.hpp"
 
+enum class Choice
+{
+    SimpleInterestRates = 0,
+    Go_Out = 1,
+};
+
+const char *ChoiceTypes[2] = {"SimpleInterestRates", "Go_Out"};
+
 int main(int argc, char **arg)
 {
-    std::cout << "Hello, from FinancialProgramming! " << calculateSimpleInterestRate(100, 1);
+    Choice choice = Choice::SimpleInterestRates;
+    int from;
+
+    while (true)
+    {
+        std::cout << "Enter the choice: \n\t"
+                  << ChoiceTypes[(int)Choice::SimpleInterestRates] << ": " << (int)Choice::SimpleInterestRates << "\n\t"
+                  << ChoiceTypes[(int)Choice::Go_Out] << ": " << (int)Choice::Go_Out;
+
+        std::cout << "\n\n";
+
+        std::cin >> from;
+
+        choice = (Choice)from;
+
+        if (choice == Choice::Go_Out)
+        {
+            std::cout << "Go_Out\n";
+            break;
+        }
+
+        switch (choice)
+        {
+        case Choice::SimpleInterestRates:
+            std::cout << "SimpleInterestRates: " << calculateSimpleInterestRate(100, 1) << "\n";
+            break;
+        default:
+            std::cout << "Invalid Selection\n";
+            break;
+        }
+    }
 
     return 0;
 }
